111cbeeda61133c6e34734e5228b2360ab3b5e98
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2026-04-10 23:16:59 -0300

    Start project

---
+++ src/test/java/br/com/latanks/cidasdepilacao_api/CidasdepilacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.latanks.cidasdepilacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidasdepilacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
